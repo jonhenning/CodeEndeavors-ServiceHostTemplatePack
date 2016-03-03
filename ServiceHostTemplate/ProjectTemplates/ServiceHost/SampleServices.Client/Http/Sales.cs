5a245234f77d0ed1e8bc19b723af46bcb0638654
jonhenning <jonhenning@gmail.com>
2016-03-03 09:09:46 -0600

    additional usings for Generics and references to ComponentModel

--- ServiceHostTemplate/ProjectTemplates/ServiceHost/SampleServices.Client/Http/Sales.cs
+++ ServiceHostTemplate/ProjectTemplates/ServiceHost/SampleServices.Client/Http/Sales.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using CodeEndeavors.ServiceHost.Common.Services;
 using CodeEndeavors.ServiceHost.Common.Services.LoggingServices;
 using DomainObjects = $saferootprojectname$.Shared.DomainObjects;
--- ServiceHostTemplate/ProjectTemplates/ServiceHost/SampleServices.Client/Stubs/Sales.cs
+++ ServiceHostTemplate/ProjectTemplates/ServiceHost/SampleServices.Client/Stubs/Sales.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using CodeEndeavors.ServiceHost.Common.Services;
 
 namespace $saferootprojectname$.Client.Stubs
