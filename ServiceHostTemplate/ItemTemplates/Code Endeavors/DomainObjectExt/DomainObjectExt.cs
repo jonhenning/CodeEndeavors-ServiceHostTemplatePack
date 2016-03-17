ebdcb9d5a1460a84a5ef2714178687e333b59128
Aaron Brackmann <abrackmann@gmail.com>
2016-03-17 10:54:43 -0500

    FIX: Added .DomainObjects to DomainObjectExt so it can find references

--- ServiceHostTemplate/ItemTemplates/Code Endeavors/DomainObjectExt/DomainObjectExt.cs
+++ ServiceHostTemplate/ItemTemplates/Code Endeavors/DomainObjectExt/DomainObjectExt.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace $defaultnamespace$
+namespace $defaultnamespace$.DomainObjects
 {
     public partial class $originalsafeitemname$ 
     {
